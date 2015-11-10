2c13266b63843bbee6e34891ddcc8c0c9e71d782
Trey <thoover.mail@gmail.com>
2015-11-10 00:58:54 -0500

    Improve prompt

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ var prompt = require('prompt');
 var pl = require('pluralize');
 var DICTIONARY_WORDS = fs.readFileSync('./dictionary.txt', 'utf-8').split('\n');
 
+prompt.message = '';
+prompt.delimiter = '';
+prompt.colors = false;
 prompt.start();
 
 main();
 
 function main() {
-  prompt.get('word', function (err, result) {
+  prompt.get({
+    properties: {
+      word: {
+        description: 'Word:'
+      }
+    }
+  }, function (err, result) {
     if (err) return;
     var anagrams = findAnagrams(result['word']);
     var n = anagrams.length;
     console.log(`Found ${n} matching ${pl('anagrams', n)}:`, anagrams);
-    main();
+
+    prompt.get({
+      properties: {
+        again: {
+          description: 'Again? (Y/n)'
+        }
+      }
+    }, function (err, result) {
+      if (err) return;
+      return result['again'][0].toLowerCase() != 'n' ? main() : goodbye();
+    });
   });
 }
 
@@ -24,4 +43,8 @@ function findAnagrams(word) {
           .filter(w => w.length === word.length && w !== word)
           .filter(w => w.split('').sort().join() === sortedWord)
           .value();
+}
+
+function goodbye() {
+  console.log('Thanks for using anagram solver!');
 }
